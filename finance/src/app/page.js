bc008c9576f21ac8244712505957e04f39a493f0
scrap-devs <lucasung07@gmail.com>
2025-02-25 18:29:08 +0000

    add-ons

--- finance/src/app/page.js
+++ finance/src/app/page.js
@@ -1,9 +1,29 @@
 import styles from "./page.module.css";
+import Cards from "./Cards.jsx";
 
 export default function Home() {
   return (
     <div>
-      test
+      <div className={styles.welcome_container}>
+        <div className={styles.welcome_left}>
+          <div>
+            Welcome User!
+          </div>
+          <div>
+            Wager
+          </div>
+          <div>
+            Profit
+          </div>
+        </div>
+        <div className={styles.welcome_right}>
+          Placement
+        </div>
+      </div>
+      <div className={styles.recommended_games}>
+        <Cards name="Blackjack" image="https://play-lh.googleusercontent.com/009hpXoLRxULWBEF8VsHnNTjFrOQVFKfkQfIxZcDGWtVSZEU5mKtSJyy3Zv3pxVcZQ" />
+        <Cards name="Plinko" image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSDs3AioTT6XtxOS1nwXtmcrSR4Rh5xWZ2E0g&s" />
+      </div>
     </div>
   );
 }
