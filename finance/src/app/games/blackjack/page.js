911b9a7ca9f3a692c1813ed51c6a1287d5c8c562
dani-72 <danidiscord72@gmail.com>
2025-03-13 13:34:07 -0400

    .

--- finance/src/app/games/blackjack/page.js
+++ finance/src/app/games/blackjack/page.js
@@ -1,4 +1,6 @@
 import "./style.css"
+import Image from "next/image"
+import cardBack from "../../../../public/cards/BACK.png"
 
 let dealerSum = 0;
 let yourSum = 0;
@@ -8,9 +10,7 @@ let hidden;
 let deck;
 let canHit = true;
 
-window.onload = function(){
-    buildDeck();
-}
+
 
 function buildDeck(){
     let values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
@@ -24,14 +24,18 @@ function buildDeck(){
     }
 }
 
- 
 
 export default function Blackjack(){
     return (
         <body>
             <h2>Dealer: <span id={"dealer-sum"}></span></h2>
             <div id="dealer-cards">
-                <img id="hidden" src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fcommons.wikimedia.org%2Fwiki%2FFile%3ACard_back_01.svg&psig=AOvVaw1HElOs8KOKSKDzsSV1CHAu&ust=1741288998143000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCOid4abV84sDFQAAAAAdAAAAABAE" alt="Back of Card" />
+                <Image
+                    src={cardBack}
+                    width={500}
+                    height={500}
+                    alt="back card"
+                />
             </div>
                     
 
