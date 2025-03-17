3e88d697ebf2bf0be450450f9a5f5e483f9c9948
uspilledtea <119764073+uspilledtea@users.noreply.github.com>
2025-03-17 15:35:32 +0000

    fixed register page

--- finance/next.config.mjs
+++ finance/next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
+const nextConfig = {
+    experimental: {
+    serverActions: {
+    allowedOrigins: [
+    "https://turbo-space-lamp-9vqr4qw7x57hp4q-3000.app.github.dev/",
+    "localhost:3000",
+    ],
+    },
+    mdxRs: true,
+    },
+    };
 export default nextConfig;
