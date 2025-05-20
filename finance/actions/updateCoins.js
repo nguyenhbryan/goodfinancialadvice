b3b8ca8afc78d619a380994d2e2ef84f8a7f01f5
Bryan Nguyen <nguyenhbryan0818@gmail.com>
2025-05-20 15:37:10 -0400

    user validation

--- finance/actions/updateCoins.js
+++ finance/actions/updateCoins.js
@@ -7,6 +7,13 @@ export async function updateCoins(username, amount) {
     await connectDB();
     try {
         // Increment the coins using Mongoose's $inc operator
+        const user = await User.findOne({ name: username });
+        if (!user) {
+            throw new Error('User not found', username);
+        }
+        if (user.coins + amount < 0) {
+            throw new Error('Insufficient coins');
+        }
         const updatedUser = await User.findOneAndUpdate(
             { name: username },
             { $inc: { coins: amount } },
